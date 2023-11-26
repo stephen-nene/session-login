145ff9c140f9b0b3209ef035184f5f937a31b2c3
stephen-nene <stevekid705@mail.com>
2023-11-26 10:13:39 +0300

    new code

---
+++ README.md
@@ -0,0 +1,1 @@
+# Markdairyfresh
---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Step 1: Navigate to the client directory
+echo "Step 1: Navigating to the client directory..."
+cd client
+
+# Step 2: Building the client application
+echo "Step 2: Building the client application..."
+npm run build
+
+# Step 3: Returning to the main project directory
+echo "Step 3: Returning to the main project directory..."
+cd ..
+
+# Step 4: Copying assets from the client's dist directory to the API's public directory
+echo "Step 4: Copying assets from the client's dist directory to the API's public directory..."
+cp -r client/dist/* api/public/
+
+# Completion: Script finished!
+echo "Script completed. Assets have been copied to the API's public directory."
+
+
+# write a script that can run git add , git commit git push and fetch first
+# first ask if you used git flow or not and initiate a git flow else
+# if terminal goes into hybernation do cmatrix
